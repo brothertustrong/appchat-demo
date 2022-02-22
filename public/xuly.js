23c4518fd9dbc830d4571abbb53e60c00c75f528
brothertustrong <taanhtu1999@gmail.com>
2022-02-22 23:29:20 +0700

    haha

---
+++ public/xuly.js
@@ -0,0 +1,61 @@
+var socket = io("https://chat-demo-taanhtu.herokuapp.com");
+
+// var socket = io("http://localhost:3000");
+
+socket.on("server-send-dki-thatbai", function(){
+    alert("có người dk ten nay roi !!!");
+});
+
+socket.on("server-send-dki-thanhcong", function(data){
+    $("#currentUser").html(data);
+    $("#loginForm").hide(2000);
+    $("#chatForm").show(1000);
+});
+
+socket.on("server-send-danhsach-Users", function(data){
+    $("#boxContent").html("");
+    data.forEach(element => {
+        $("#boxContent").append("<div class='user'>" + element + "</div>");
+    });
+});
+
+socket.on("server-send-message", function(data){
+    $("#listMessage").append("<div class='ms'>"+ data.un + ":" + data.nd+"</div>");
+});
+
+socket.on("ai-do-dang-go-chu", function(data){
+    $("#thongbao").html(data + "<img width=20px src='typing.gif'>");
+});
+
+socket.on("ai-do-stop-go-chu", function(){
+    $("#thongbao").html("");
+});
+
+$(document).ready(function(){
+    $("#loginForm").show();
+    $("#chatForm").hide();
+
+    $("#btnRegister").click(function(){
+        socket.emit("client-send-Username", $("#txtUserName").val());
+    });
+
+    $("#btnLogout").click(function(){
+        socket.emit("logout");
+        $("#loginForm").show(1000);
+        $("#chatForm").hide(2000);
+    });
+
+    $("#btnSend").click(function(){
+        socket.emit("user-send-message", $("#txtMessage").val());
+    });
+
+    $("#txtMessage").focusin(function(){
+        socket.emit("toi-dang-go-chu");
+    });
+
+    $("#txtMessage").focusout(function(){
+        socket.emit("toi-stop-go-chu");
+    });
+});
+// khi tải trang web thì phần login hiện ra và phần chat ấn đi
+
---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(process.env.PORT || 3000);
+// server.listen(3000);
+
+var mangUser=[];
+
+io.on("connection", function(socket){
+    console.log("co nguoi ket noi len server " + socket.id);
+
+    socket.on("client-send-Username", function(data){
+        //console.log(data);
+        if(mangUser.indexOf(data)>=0){
+            socket.emit("server-send-dki-thatbai");
+        }else{
+            mangUser.push(data);
+            socket.Username=data;
+            socket.emit("server-send-dki-thanhcong", data);
+            io.sockets.emit("server-send-danhsach-Users", mangUser);
+        }
+    });
+
+    socket.on("logout", function(){
+        mangUser.splice(
+            mangUser.indexOf(socket.Username), 1
+        );
+
+        socket.broadcast.emit("server-send-danhsach-Users", mangUser);
+    });
+
+    socket.on("user-send-message", function(data){
+        io.sockets.emit("server-send-message", {un:socket.Username, nd: data});
+    });
+
+    socket.on("toi-dang-go-chu", function(){
+        //console.log(socket.Username + " dang go chu");
+        var s = socket.Username + " dang go chu";
+        io.sockets.emit("ai-do-dang-go-chu", s);
+    });
+
+    socket.on("toi-stop-go-chu", function(){
+        //console.log(socket.Username + " stop go chu");
+        io.sockets.emit("ai-do-stop-go-chu");
+    });
+});
+
+app.get("/", function(req, res){
+    res.render("trangchu1");
+});
